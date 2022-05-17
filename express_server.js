4d2019cb60e243b3a544076445b40e0e459ab2d6
Ali <alihbouran@gmail.com>
2022-05-16 19:51:43 -0700

    implemented a function that returns random 6 characters

--- express_server.js
+++ express_server.js
@@ -2,12 +2,32 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const createShortURL = function generateRandomString() {
+  let randomString = [];
+  const characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  
+  for (let j = 0; j < 60; j++) {
+    randomString.push(characters[Math.floor(Math.random() * 60)]);
+  }
+  
+  randomString = randomString.slice(0, 6);
+  randomString = randomString.join('');
+  return randomString;
+};
+
 app.set('view engine', 'ejs');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
+
+app.post('/urls', (req, res) => {
+  console.log('request body:', req.body);
+  res.send('Ok');
+})
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -31,6 +51,7 @@ app.get('/urls/:shortURL', (req, res) => {
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
