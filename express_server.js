b67cabb5c8c5d85eedfa3f2f4400e68afbf036d8
Ali <alihbouran@gmail.com>
2022-05-18 16:56:56 -0700

    Implemented logout and login  and cookies

--- express_server.js
+++ express_server.js
@@ -8,10 +8,10 @@ app.use(morgan('dev'));
 app.set('view engine', 'ejs');
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 const cookieParser = require('cookie-parser')
-
+app.use(cookieParser());
 
 function generateRandomString() {
   let randomString = [];
@@ -30,6 +30,10 @@ function generateRandomString() {
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
+};
+
+const user = {
+  
 };
 
 app.get('/', (req, res) => {
@@ -58,7 +62,10 @@ app.post('/urls', (req, res) => {
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  const templateVars = { 
+    username: req.cookies['username']
+  };
+  res.render('urls_new', templateVars);
 });
 
 
@@ -84,10 +91,16 @@ app.get('/u/:shortURL', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  let individualUsername = req.body.username;
-  console.log(individualUsername)
-  res.cookie('username', individualUsername)
-  res.redirect('/urls')
+  const value = req.body.username;
+  res.cookie('username', value);
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+  res.cookie('username', null);
+  console.log('Hello')
+  res.clearCookie('username');
+  res.redirect('/urls');
 });
 
 app.get('/hello', (req, res) => {
